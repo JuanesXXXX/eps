8d861f6f2e55eda4caca1f39d2efabc91c24557b
JuanesXXXX <124816565+JuanesXXXX@users.noreply.github.com>
2023-04-19 13:10:24 -0500

    Add files via upload

---
+++ GUIA4/src/guia4/jList1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guia4;
+
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author ESTUDIANTE 25
+ */
+class jList1 {
+
+    static void setModel(DefaultListModel modelo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
